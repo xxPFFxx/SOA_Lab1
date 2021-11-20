d3d294407671dd48e551d31453d9c821b11e2f89
Daniil Demichev <daniil_demichev@mail.ru>
2021-11-20 13:40:15 +0300

    2/3 additional tasks working

---
+++ src/main/java/dto/CountDTO.java
@@ -0,0 +1,15 @@
+package dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+
+@NoArgsConstructor
+public class CountDTO {
+    private Long count;
+}
---
+++ src/main/java/servlets/AdditionalTasksServlet.java
@@ -0,0 +1,74 @@
+package servlets;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import dto.CountDTO;
+import dto.PagedHumanBeingList;
+import dto.dtoList.HumanBeingDTOList;
+import mapper.HumanBeingMapper;
+import models.HumanBeing;
+import models.WeaponType;
+import org.hibernate.Session;
+import org.hibernate.query.Query;
+import util.HibernateUtil;
+
+import javax.persistence.EntityManager;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@WebServlet("/additional")
+public class AdditionalTasksServlet extends HttpServlet {
+    private Session session;
+    private EntityManager em;
+    private Gson gson;
+    private HumanBeingMapper humanBeingMapper;
+
+    @Override
+    public void init() throws ServletException {
+        session = HibernateUtil.getSessionFactory().openSession();
+        em = session.getEntityManagerFactory().createEntityManager();
+        gson = new GsonBuilder().setPrettyPrinting().create();
+        humanBeingMapper = new HumanBeingMapper();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String weaponTypeCount = request.getParameter("weaponTypeCount");
+        String weaponTypeArray = request.getParameter("weaponTypeArray");
+
+        CriteriaBuilder criteriaBuilder = em.getCriteriaBuilder();
+        CriteriaQuery<Long> countQuery = criteriaBuilder.createQuery(Long.class);
+        CriteriaQuery<HumanBeing> criteriaQuery = criteriaBuilder.createQuery(HumanBeing.class);
+        Root<HumanBeing> from = criteriaQuery.from(HumanBeing.class);
+        countQuery.select(criteriaBuilder.count(countQuery.from(HumanBeing.class)));
+        em.createQuery(countQuery);
+
+        if (weaponTypeCount != null) {
+            countQuery.where(criteriaBuilder.greaterThan(from.get("weaponType"), WeaponType.valueOf(weaponTypeCount)));
+            Long countResult = em.createQuery(countQuery).getSingleResult();
+            CountDTO countDTO = new CountDTO();
+            countDTO.setCount(countResult);
+            response.getWriter().write(gson.toJson(countDTO));
+            return;
+        }
+        if (weaponTypeArray != null) {
+            criteriaQuery.where(criteriaBuilder.greaterThan(from.get("weaponType"), WeaponType.valueOf(weaponTypeArray)));
+            List<HumanBeing> humanBeingList = em.createQuery(criteriaQuery).getResultList();
+            PagedHumanBeingList pagedHumanBeingList = new PagedHumanBeingList();
+            pagedHumanBeingList.setHumanBeingList(humanBeingList);
+            pagedHumanBeingList.setCount(Long.parseLong(String.valueOf(humanBeingList.size())));
+            HumanBeingDTOList dto = new HumanBeingDTOList((humanBeingMapper.mapHumanBeingListToHumanBeingDTOList(pagedHumanBeingList.getHumanBeingList())), pagedHumanBeingList.getCount());
+            response.getWriter().write(gson.toJson(dto));
+            return;
+        }
+    }
+}
